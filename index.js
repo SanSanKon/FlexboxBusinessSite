53652e38aa2f121bcf896c6a39dfe87814ce32ca
SanSanKon <kaba100@yandex.ru>
2022-12-04 10:35:46 +0100

    refactored index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navigation-list');
 const navLinks = document.querySelectorAll('.navigation-list li');
-
+ 
 const navSlide = () => {
    
     burger.addEventListener('click', ()=> {
@@ -25,95 +25,77 @@ const navSlide = () => {
 
 navSlide();
 
-    navLinks.forEach(link => link.addEventListener('click', ()=> {
-        burger.classList.toggle('toggle');
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-    }));
+navLinks.forEach(link => link.addEventListener('click', ()=> {
+    burger.classList.toggle('toggle');
+    nav.classList.toggle('nav-active');
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    });
+}));
 
-// document.onclick = (e) => {
-//     if(e.target !== nav && e.target !== burger) {
-//         nav.classList.remove('nav-active');
-//         burger.classList.remove('toggle');
-//     }
-// }
-
-
+//To show more info about North
+class moreInfoParagraph {
+    constructor(selector) {
+        this.selector = document.querySelector(selector);
+    }
+    showMoreInfo(toggle = false) {
+        if (toggle && this.selector.classList.contains('display-paragraph')) {
+            this.selector.classList.remove('display-paragraph');
+        } else {
+            this.selector.classList.add('display-paragraph');
+        }
+    }
+};
 
 //Event handler for powerful property
-
-const paragraphPowerful = document.querySelector('#powerful');
-const powerfulMoreInfo = document.querySelector('.powerful-more-info');
-
-const displayMoreInfoPowerful = () => {
-    powerfulMoreInfo.classList.toggle('display-paragraph');
-}
-
-paragraphPowerful.addEventListener('click', displayMoreInfoPowerful);
-
+const powerful = document.querySelector('#powerful');
+const powerMoreInfo = new moreInfoParagraph('.powerful-more-info');
+powerful.addEventListener('click', () => powerMoreInfo.showMoreInfo(true));
 
 //Event handler for majestic property
-const paragraphMajestic = document.querySelector('#majestic');
-const majesticMoreInfo = document.querySelector('.majestic-more-info');
-
-const displayMoreInfoMajestic = () => {
-    majesticMoreInfo.classList.toggle('display-paragraph');
-}
-
-paragraphMajestic.addEventListener('click', displayMoreInfoMajestic);
+const majestic = document.querySelector('#majestic');
+const majesticMoreInfo = new moreInfoParagraph('.majestic-more-info');
+majestic.addEventListener('click', () => majesticMoreInfo.showMoreInfo(true));
 
 //Event handler for stern property
-const paragraphStern = document.querySelector('#stern');
-const sternMoreInfo = document.querySelector('.stern-more-info');
-
-const displayMoreInfoStern = () => {
-    sternMoreInfo.classList.toggle('display-paragraph');
-}
-
-paragraphStern.addEventListener('click', displayMoreInfoStern);
+const stern = document.querySelector('#stern');
+const sternMoreInfo = new moreInfoParagraph('.stern-more-info');
+stern.addEventListener('click', () => sternMoreInfo.showMoreInfo(true));
 
 //Event handler for secluded property
-const paragraphSecluded = document.querySelector('#secluded');
-const secludedMoreInfo = document.querySelector('.secluded-more-info');
+const secluded = document.querySelector('#secluded');
+const secludedMoreInfo = new moreInfoParagraph('.secluded-more-info');
+secluded.addEventListener('click', () => secludedMoreInfo.showMoreInfo(true));
 
-const displayMoreInfoSecluded = () => {
-    secludedMoreInfo.classList.toggle('display-paragraph');
-}
-
-paragraphSecluded.addEventListener('click', displayMoreInfoSecluded);
+//To show more info about houses
+class moreInfoHouse {
+    constructor(selector) {
+        this.selector = document.querySelector(selector);
+    }
+    showMoreInfoHouse(toggle = false) {
+        if (toggle && this.selector.classList.contains('display-paragraph-house')) {
+            this.selector.classList.remove('display-paragraph-house');
+        } else {
+            this.selector.classList.add('display-paragraph-house');
+        }
+    }
+};
 
 //Event handler for house-one
 const houseOne = document.querySelector('#house-one');
-const houseOneMoreInfo = document.querySelector('.house-one-more-info');
-
-const displayMoreInfoHouseOne = () => {
-    houseOneMoreInfo.classList.toggle('display-paragraph-house');
-}
-
-houseOne.addEventListener('click', displayMoreInfoHouseOne);
+const houseOneMoreInfo = new moreInfoHouse('.house-one-more-info');
+houseOne.addEventListener('click', () => houseOneMoreInfo.showMoreInfoHouse(true));
 
 //Event handler for house-two
 const houseTwo = document.querySelector('#house-two');
-const houseTwoMoreInfo = document.querySelector('.house-two-more-info');
-
-const displayMoreInfoHouseTwo = () => {
-    houseTwoMoreInfo.classList.toggle('display-paragraph-house');
-}
-
-houseTwo.addEventListener('click', displayMoreInfoHouseTwo);
+const houseTwoMoreInfo = new moreInfoHouse('.house-two-more-info');
+houseTwo.addEventListener('click', () => houseTwoMoreInfo.showMoreInfoHouse(true));
 
 //Event handler for house-three
 const houseThree = document.querySelector('#house-three');
-const houseThreeMoreInfo = document.querySelector('.house-three-more-info');
-
-const displayMoreInfoHouseThree = () => {
-    houseThreeMoreInfo.classList.toggle('display-paragraph-house');
-}
-
-houseThree.addEventListener('click', displayMoreInfoHouseThree);
+const houseThreeMoreInfo = new moreInfoHouse('.house-three-more-info');
+houseThree.addEventListener('click', () => houseThreeMoreInfo.showMoreInfoHouse(true));
